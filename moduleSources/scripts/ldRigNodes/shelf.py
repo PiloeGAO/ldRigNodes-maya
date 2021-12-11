8e129d67e2967978567a8d578dd1a67858c4d440
PiloeGAO <leonumerique@gmail.com>
2021-12-11 16:17:04 +0100

    ADD: Refactor the display of the shelf

--- moduleSources/scripts/ldRigNodes/shelf.py
+++ moduleSources/scripts/ldRigNodes/shelf.py
@@ -27,32 +27,32 @@ def create_shelf():
    shelfStyle = ('shelf' if maya_version >= 2016 else 'simple')
 
    # Alignment Utils.
-   cmds.shelfButton(label='Align to Guids', command='from ldRigNodes.shelf_commands import align_to_guids; align_to_guids()', sourceType='python', annotation='', image='align_to_guids.png', style='iconOnly')
-   cmds.shelfButton(label='Scale Guids', command='from ldRigNodes.shelf_commands import scale_guids; scale_guids()', sourceType='python', annotation='', image='scale_guids.png', style='iconOnly')
-   cmds.shelfButton(label='Scale Controllers', command='from ldRigNodes.shelf_commands import scale_controllers; scale_controllers()', sourceType='python', annotation='', image='scale_controllers.png', style='iconOnly')
+   cmds.shelfButton(label='Align to GUIDs', command='from ldRigNodes.shelf_commands import align_to_guids; align_to_guids()', sourceType='python', annotation='', image='menuList.png', style='iconOnly')
+   cmds.shelfButton(label='Scale Guids', command='from ldRigNodes.shelf_commands import scale_guids; scale_guids()', sourceType='python', annotation='', imageOverlayLabel='GUID', image='larger.png', style='iconAndTextHorizontal')
+   cmds.shelfButton(label='Scale Controllers', command='from ldRigNodes.shelf_commands import scale_controllers; scale_controllers()', sourceType='python', annotation='', imageOverlayLabel='CTRL', image='larger.png', style='iconAndTextHorizontal')
 
    cmds.separator(width=12,height=35, style=shelfStyle, hr=False)
 
    # Bones Creators.
-   cmds.shelfButton(label='Auto Bones Generator', command='from ldRigNodes.shelf_commands import auto_bone_generator; auto_bone_generator()', sourceType='python', annotation='', image='auto_bone_generator.png', style='iconOnly')
-   cmds.shelfButton(label='Tweakers Generator', command='from ldRigNodes.shelf_commands import tweakers_generator; tweakers_generator()', sourceType='python', annotation='', image='tweakers_generator.png', style='iconOnly')
+   cmds.shelfButton(label='Auto Bones Generator', command='from ldRigNodes.shelf_commands import auto_bone_generator; auto_bone_generator()', sourceType='python', annotation='', image='contrast.png', style='iconOnly')
+   cmds.shelfButton(label='Tweakers Generator', command='from ldRigNodes.shelf_commands import tweakers_generator; tweakers_generator()', sourceType='python', annotation='', image='singleplayer.png', style='iconOnly')
    
    cmds.separator(width=12,height=35, style=shelfStyle, hr=False)
 
    # Attributes Management.
-   cmds.shelfButton(label='Copy And Link Attributes', command='from ldRigNodes.shelf_commands import copy_and_link_attributes; copy_and_link_attributes()', sourceType='python', annotation='', image='copy_and_link_attributes.png', style='iconOnly')
-   cmds.shelfButton(label='Copy Attributes', command='from ldRigNodes.shelf_commands import copy_custom_attribute; copy_custom_attribute()', sourceType='python', annotation='', image='copy_custom_attribute.png', style='iconOnly')
-   cmds.shelfButton(label='Paste Attributes', command='from ldRigNodes.shelf_commands import paste_custom_attribute; paste_custom_attribute()', sourceType='python', annotation='', image='paste_custom_attribute.png', style='iconOnly')
+   cmds.shelfButton(label='Copy And Link Attributes', command='from ldRigNodes.shelf_commands import copy_and_link_attributes; copy_and_link_attributes()', sourceType='python', annotation='', image='key.png', style='iconOnly')
+   cmds.shelfButton(label='Copy Attributes', command='from ldRigNodes.shelf_commands import copy_custom_attribute; copy_custom_attribute()', sourceType='python', annotation='', image='export.png', style='iconOnly')
+   cmds.shelfButton(label='Paste Attributes', command='from ldRigNodes.shelf_commands import paste_custom_attribute; paste_custom_attribute()', sourceType='python', annotation='', image='import.png', style='iconOnly')
 
    cmds.separator(width=12,height=35, style=shelfStyle, hr=False)
 
    # Clear namespaces.
-   cmds.shelfButton(label='Clear namespaces', command='from ldRigNodes.shelf_commands import clear_namespaces; clear_namespaces()', sourceType='python', annotation='', image='clear_namespaces.png', style='iconOnly')
+   cmds.shelfButton(label='Clear namespaces', command='from ldRigNodes.shelf_commands import clear_namespaces; clear_namespaces()', sourceType='python', annotation='', image='trashCanOpen.png', style='iconOnly')
 
    cmds.separator(width=12,height=35, style=shelfStyle, hr=False)
 
    # Space Switches Management.
-   cmds.shelfButton(label='Open Space Switch Manager', command='from ldRigNodes.space_switch_manager import open_ui; open_ui(); ', sourceType='python', annotation='', image='ssm.png', style='iconOnly')
+   cmds.shelfButton(label='Open Space Switch Manager', command='from ldRigNodes.space_switch_manager import open_ui; open_ui(); ', sourceType='python', annotation='', image='locked.png', style='iconOnly')
    if(get_autobuilder_config() != None):
-      cmds.shelfButton(label='Build Space Switch', command='from ldRigNodes.space_switch_auto_builder import autobuild_hierarchy; autobuild_hierarchy(); ', sourceType='python', annotation='', image='autobuild_ss.png', style='iconOnly')
-      cmds.shelfButton(label='Clear Space Switch', command='from ldRigNodes.space_switch_auto_builder import autoclear_hierarchy; autoclear_hierarchy(); ', sourceType='python', annotation='', image='autoclear_ss.png', style='iconOnly')
+      cmds.shelfButton(label='Build Space Switch', command='from ldRigNodes.space_switch_auto_builder import autobuild_hierarchy; autobuild_hierarchy(); ', sourceType='python', annotation='', image='share2.png', style='iconOnly')
+      cmds.shelfButton(label='Clear Space Switch', command='from ldRigNodes.space_switch_auto_builder import autoclear_hierarchy; autoclear_hierarchy(); ', sourceType='python', annotation='', image='toolEraser.png', style='iconOnly')
