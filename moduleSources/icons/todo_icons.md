c567b0e48f06479807fc91eb26b0ca8857d6b5ef
PiloeGAO <leonumerique@gmail.com>
2022-02-22 13:52:44 +0100

    ADD: Add custom icons for the toolbar

--- moduleSources/icons/todo_icons.md
+++ moduleSources/icons/todo_icons.md
@@ -2,27 +2,29 @@
 
 ## Toolbar
 
-[ ] Align to  GUID
-[ ] Scale GUID/CON
-[ ] Align chain of objects
-[ ] Module align from namespace
-[ ] Change Rotate Orders
-[ ] Auto Bones Generator
-[ ] Tweakers Generator
-[ ] Hairs Generator
-[ ] Copy Display
-[ ] Paste Display
-[ ] Switch Display (Anim/Rig)
-[ ] Copy and Link Attributes
-[ ] Copy Attributes
-[ ] Paste Attributes
-[ ] Clear Namespaces
-[ ] Apply bones from NGSkinTools
-[ ] Select Bones from selected Module
-[ ] Rename objects (set correct index)
-[ ] Space Switch Manager
-[ ] Auto Build Space Switches
-[ ] Clear Space Switches
+[X] Align to  GUID
+[X] Scale GUID/CON
+[X] Align chain of objects
+[X] Module align from namespace
+[X] Change Rotate Orders
+[X] Auto Bones Generator
+[X] Tweakers Generator
+[X] Hairs Generator
+[X] Mid Buffer Creator
+[X] Facial Curves Creator
+[X] Copy Display
+[X] Paste Display
+[X] Switch Display (Anim/Rig)
+[X] Copy and Link Attributes
+[X] Copy Attributes
+[X] Paste Attributes
+[X] Clear Namespaces
+[X] Apply bones from NGSkinTools
+[X] Select Bones from selected Module
+[X] Rename objects (set correct index)
+[X] Space Switch Manager
+[X] Auto Build Space Switches
+[X] Clear Space Switches
 
 ## PySide2 Windows
 ### Space Switch Manager
--- moduleSources/scripts/ldRigNodes/shelf.py
+++ moduleSources/scripts/ldRigNodes/shelf.py
@@ -8,7 +8,7 @@
 import maya.cmds as cmds
 import maya
 
-from ldRigNodes.space_switch_auto_builder import get_autobuilder_config
+# from ldRigNodes.space_switch_auto_builder import get_autobuilder_config
 
 def remove_shelf():
    if cmds.shelfLayout('ldRigNodes', exists=True):
@@ -27,52 +27,52 @@ def create_shelf():
    shelfStyle = ('shelf' if maya_version >= 2016 else 'simple')
 
    # Alignment Utils.
-   cmds.shelfButton(label='Align to GUIDs', command='from ldRigNodes.shelf_commands import align_to_guids; align_to_guids()', sourceType='python', annotation='', image='old/menuList.png', style='iconOnly')
-   cmds.shelfButton(label='Scale Guids', command='from ldRigNodes.shelf_commands import scale_guids; scale_guids()', sourceType='python', annotation='', imageOverlayLabel='GUID', image='old/larger.png', style='iconAndTextHorizontal')
-   cmds.shelfButton(label='Scale Controllers', command='from ldRigNodes.shelf_commands import scale_controllers; scale_controllers()', sourceType='python', annotation='', imageOverlayLabel='CTRL', image='old/larger.png', style='iconAndTextHorizontal')
-   cmds.shelfButton(label='Align Chain of Object', command='from ldRigNodes.shelf_commands import align_chain_objects; align_chain_objects()', sourceType='python', annotation='', image='old/signal3.png', style='iconOnly')
-   cmds.shelfButton(label='Align Selection With Other Namespace', command='from ldRigNodes.shelf_commands import align_sel_with_other_namespace; align_sel_with_other_namespace()', sourceType='python', annotation='', image='old/multiplayer.png', style='iconOnly')
-   cmds.shelfButton(label='Change Rotate Order', command='from ldRigNodes.shelf_commands import updateRotateOrderSelection; updateRotateOrderSelection()', sourceType='python', annotation='', image='old/fightJoy_25.png', style='iconOnly')
+   cmds.shelfButton(label='Align to GUIDs', command='from ldRigNodes.shelf_commands import align_to_guids; align_to_guids()', sourceType='python', annotation='', image='AlignToGuid1.png', style='iconOnly')
+   cmds.shelfButton(label='Scale Guids', command='from ldRigNodes.shelf_commands import scale_guids; scale_guids()', sourceType='python', annotation='', imageOverlayLabel='GUID', image='Scale2.png', style='iconAndTextHorizontal')
+   cmds.shelfButton(label='Scale Controllers', command='from ldRigNodes.shelf_commands import scale_controllers; scale_controllers()', sourceType='python', annotation='', imageOverlayLabel='CTRL', image='Scale2.png', style='iconAndTextHorizontal')
+   cmds.shelfButton(label='Align Chain of Object', command='from ldRigNodes.shelf_commands import align_chain_objects; align_chain_objects()', sourceType='python', annotation='', image='AlignChainOfObjects3.png', style='iconOnly')
+   cmds.shelfButton(label='Align Selection With Other Namespace', command='from ldRigNodes.shelf_commands import align_sel_with_other_namespace; align_sel_with_other_namespace()', sourceType='python', annotation='', image='ModuleAlignNamespace4.png', style='iconOnly')
+   cmds.shelfButton(label='Change Rotate Order', command='from ldRigNodes.shelf_commands import updateRotateOrderSelection; updateRotateOrderSelection()', sourceType='python', annotation='', image='ChangeRotateOrders5.png', style='iconOnly')
 
    cmds.separator(width=12,height=35, style=shelfStyle, hr=False)
 
    # Bones Creators.
-   cmds.shelfButton(label='Auto Bones Generator', command='from ldRigNodes.shelf_commands import auto_bone_generator; auto_bone_generator()', sourceType='python', annotation='', image='old/contrast.png', style='iconOnly')
-   cmds.shelfButton(label='Tweakers Generator', command='from ldRigNodes.shelf_commands import tweakers_generator; tweakers_generator()', sourceType='python', annotation='', imageOverlayLabel='TWEAK', image='old/singleplayer.png', style='iconOnly')
-   cmds.shelfButton(label='Hairs Generator', command='from ldRigNodes.shelf_commands import create_fk_hairs; create_fk_hairs()', sourceType='python', annotation='', imageOverlayLabel='HAIRS', image='old/singleplayer.png', style='iconOnly')
+   cmds.shelfButton(label='Auto Bones Generator', command='from ldRigNodes.shelf_commands import auto_bone_generator; auto_bone_generator()', sourceType='python', annotation='', image='AutoBonesGen6.png', style='iconOnly')
+   cmds.shelfButton(label='Tweakers Generator', command='from ldRigNodes.shelf_commands import tweakers_generator; tweakers_generator()', sourceType='python', annotation='', image='TweakerGen7.png', style='iconOnly')
+   cmds.shelfButton(label='Hairs Generator', command='from ldRigNodes.shelf_commands import create_fk_hairs; create_fk_hairs()', sourceType='python', annotation='', image='HairsGen8.png', style='iconOnly')
    
    cmds.separator(width=12,height=35, style=shelfStyle, hr=False)
 
    # Facial Tools.
-   cmds.shelfButton(label='Mid Buffer Creator', command='from ldRigNodes.shelf_commands import createMidBuffer; createMidBuffer()', sourceType='python', annotation='', imageOverlayLabel='BUF', image='old/barsHorizontal.png', style='iconOnly')
-   cmds.shelfButton(label='Facial Curve Creator', command='from ldRigNodes.shelf_commands import createFacialCurve; createFacialCurve()', sourceType='python', annotation='', imageOverlayLabel='CURVE', image='old/singleplayer.png', style='iconOnly')
+   cmds.shelfButton(label='Mid Buffer Creator', command='from ldRigNodes.shelf_commands import createMidBuffer; createMidBuffer()', sourceType='python', annotation='', image='MidBufferCreator9.png', style='iconOnly')
+   cmds.shelfButton(label='Facial Curve Creator', command='from ldRigNodes.shelf_commands import createFacialCurve; createFacialCurve()', sourceType='python', annotation='', image='FacialCurvesCreator10.png', style='iconOnly')
    
    cmds.separator(width=12,height=35, style=shelfStyle, hr=False)
    
    # Display Tools.
-   cmds.shelfButton(label='Copy Display', command='from ldRigNodes.shelf_commands import copy_display; copy_display()', sourceType='python', annotation='', imageOverlayLabel='DISPL', image='old/export.png', style='iconOnly')
-   cmds.shelfButton(label='Paste Display', command='from ldRigNodes.shelf_commands import paste_display; paste_display()', sourceType='python', annotation='', imageOverlayLabel='DISPL', image='old/import.png', style='iconOnly')
-   cmds.shelfButton(label='Switch Module Displays', command='from ldRigNodes.shelf_commands import switch_modules_displays; switch_modules_displays()', sourceType='python', annotation='', imageOverlayLabel='', image='old/return.png', style='iconOnly')
+   cmds.shelfButton(label='Copy Display', command='from ldRigNodes.shelf_commands import copy_display; copy_display()', sourceType='python', annotation='', image='CopyDisplay11.png', style='iconOnly')
+   cmds.shelfButton(label='Paste Display', command='from ldRigNodes.shelf_commands import paste_display; paste_display()', sourceType='python', annotation='', image='PasteDisplay12.png', style='iconOnly')
+   cmds.shelfButton(label='Switch Module Displays', command='from ldRigNodes.shelf_commands import switch_modules_displays; switch_modules_displays()', sourceType='python', annotation='', imageOverlayLabel='', image='SwitchDisplay13.png', style='iconOnly')
    
    cmds.separator(width=12,height=35, style=shelfStyle, hr=False)
 
    # Attributes Management.
-   cmds.shelfButton(label='Copy And Link Attributes', command='from ldRigNodes.shelf_commands import copy_and_link_attributes; copy_and_link_attributes()', sourceType='python', annotation='', image='old/key.png', style='iconOnly')
-   cmds.shelfButton(label='Copy Attributes', command='from ldRigNodes.shelf_commands import copy_custom_attribute; copy_custom_attribute()', sourceType='python', annotation='', image='old/export.png', style='iconOnly')
-   cmds.shelfButton(label='Paste Attributes', command='from ldRigNodes.shelf_commands import paste_custom_attribute; paste_custom_attribute()', sourceType='python', annotation='', image='old/import.png', style='iconOnly')
+   cmds.shelfButton(label='Copy And Link Attributes', command='from ldRigNodes.shelf_commands import copy_and_link_attributes; copy_and_link_attributes()', sourceType='python', annotation='', image='CopyLinkAttributes14.png', style='iconOnly')
+   cmds.shelfButton(label='Copy Attributes', command='from ldRigNodes.shelf_commands import copy_custom_attribute; copy_custom_attribute()', sourceType='python', annotation='', image='CopyAttributes15.png', style='iconOnly')
+   cmds.shelfButton(label='Paste Attributes', command='from ldRigNodes.shelf_commands import paste_custom_attribute; paste_custom_attribute()', sourceType='python', annotation='', image='PasteAttributes16.png', style='iconOnly')
 
    cmds.separator(width=12,height=35, style=shelfStyle, hr=False)
 
    # Various Utils.
-   cmds.shelfButton(label='Clear namespaces', command='from ldRigNodes.shelf_commands import clear_namespaces; clear_namespaces()', sourceType='python', annotation='', image='old/trashCanOpen.png', style='iconOnly')
-   cmds.shelfButton(label='Apply Bones for ngskintools export', command='from ldRigNodes.shelf_commands import apply_bones_from_ngexport; apply_bones_from_ngexport()', sourceType='python', annotation='', image='old/exitRight.png', style='iconOnly')
-   cmds.shelfButton(label='Select Bones from module', command='from ldRigNodes.shelf_commands import select_bones_from_module; select_bones_from_module()', sourceType='python', annotation='', image='old/share2.png', style='iconOnly')
-   cmds.shelfButton(label='Rename Objects', command='from ldRigNodes.shelf_commands import rename_objects; rename_objects()', sourceType='python', annotation='', image='old/medal2.png', style='iconOnly')
+   cmds.shelfButton(label='Clear namespaces', command='from ldRigNodes.shelf_commands import clear_namespaces; clear_namespaces()', sourceType='python', annotation='', image='ClearNamespaces17.png', style='iconOnly')
+   cmds.shelfButton(label='Apply Bones for ngskintools export', command='from ldRigNodes.shelf_commands import apply_bones_from_ngexport; apply_bones_from_ngexport()', sourceType='python', annotation='', image='ApplyBonesFromNGSKinTools18.png', style='iconOnly')
+   cmds.shelfButton(label='Select Bones from module', command='from ldRigNodes.shelf_commands import select_bones_from_module; select_bones_from_module()', sourceType='python', annotation='', image='SelectBonesForModule19.png', style='iconOnly')
+   cmds.shelfButton(label='Rename Objects', command='from ldRigNodes.shelf_commands import rename_objects; rename_objects()', sourceType='python', annotation='', image='RenameObjects20.png', style='iconOnly')
 
    cmds.separator(width=12,height=35, style=shelfStyle, hr=False)
 
    # Space Switches Management.
-   cmds.shelfButton(label='Open Space Switch Manager', command='from ldRigNodes.space_switch_manager import open_ui; open_ui(); ', sourceType='python', annotation='', image='old/locked.png', style='iconOnly')
+   cmds.shelfButton(label='Open Space Switch Manager', command='from ldRigNodes.space_switch_manager import open_ui; open_ui(); ', sourceType='python', annotation='', image='SpaceSwitchMan21.png', style='iconOnly')
    if(get_autobuilder_config() != None):
-      cmds.shelfButton(label='Build Space Switch', command='from ldRigNodes.space_switch_auto_builder import autobuild_hierarchy; autobuild_hierarchy(); ', sourceType='python', annotation='', image='old/share2.png', style='iconOnly')
-      cmds.shelfButton(label='Clear Space Switch', command='from ldRigNodes.space_switch_auto_builder import autoclear_hierarchy; autoclear_hierarchy(); ', sourceType='python', annotation='', image='old/toolEraser.png', style='iconOnly')
+      cmds.shelfButton(label='Build Space Switch', command='from ldRigNodes.space_switch_auto_builder import autobuild_hierarchy; autobuild_hierarchy(); ', sourceType='python', annotation='', image='AutoBuildSpaceSwitches22.png', style='iconOnly')
+      cmds.shelfButton(label='Clear Space Switch', command='from ldRigNodes.space_switch_auto_builder import autoclear_hierarchy; autoclear_hierarchy(); ', sourceType='python', annotation='', image='ClearSpaceSwitches23.png', style='iconOnly')
